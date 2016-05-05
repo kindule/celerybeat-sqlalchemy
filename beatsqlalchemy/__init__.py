24de4f68e6f7cbc9ff2ffb297d2a9ed907578bdf
jinge <ge.jin@woqutech.com>
2016-05-05 17:46:53 +0800

    update version info

--- beatsqlalchemy/__init__.py
+++ beatsqlalchemy/__init__.py
@@ -12,3 +12,14 @@
 #      History:
 #=============================================================================
 '''
+from collections import namedtuple
+
+version_info_t = namedtuple(
+    'version_info_t', ('major', 'minor', 'micro', 'releaselevel', 'serial'),
+)
+
+VERSION = version_info_t(1, 0, 0, '', '')
+__version__ = '{0.major}.{0.minor}.{0.micro}{0.releaselevel}'.format(VERSION)
+__author__ = 'Jin Ge'
+__contact__ = 'ge.jin@woqutech.com'
+__docformat__ = 'restructuredtext'
