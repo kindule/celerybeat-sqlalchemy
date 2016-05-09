a08124d170ffd05718191d437f3528552996efc6
jinge <ge.jin@woqutech.com>
2016-05-09 17:17:32 +0800

    修改包依赖版本

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.2',
 
     description='Celery beat use sqlalchemy',
     long_description=long_description,
@@ -73,7 +73,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['sqlalchemy', 'celery'],
+    install_requires=['sqlalchemy', 'celery<4.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
